247d2a36d19939b04e93e760b68756cb0af7fb56
Irina-Golubitsky <irina.golubitsky@gmail.com>
2021-07-29 12:09:02 -0700

    characters added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 rawgKey="ad7de449944a49eb8e4ece2f99e17776";
+gbKey="82377eacd0eaabd596437efbea6cb8449b5bd190";
 let game='';
 let textInfo="";
 $('#one-game').hide();
@@ -79,89 +80,125 @@ window.scrollTo({ top: scrollDiv, behavior: 'smooth'});
 
 }
 let gameInfo=function(gameSlug){
-   console.log(game);
-   $('#one-game').show();
-   //$('#one-game').empty();
-   //$('#socialgrep').empty();
+    console.log(game);
+    $('#game-content').empty();
+    $('#one-game').show();
+    //$('#one-game').empty();
+    //$('#socialgrep').empty();
+ 
+ 
+     apiURLlDetailed=`https://api.rawg.io/api/games/${gameSlug}?&key=${rawgKey}`;
+     fetch(apiURLlDetailed)
+         .then(function (response) {
+             response.json().then(function (data) {
+                 console.log(data);
+ 
+               
+                 // let mainDiv = $('<div>'), descDiv = $('<div>');
+                 // mainDiv.addClass("text-white");
+                 // let h2 = $('<h2>');
+                 game=data.name;
+                 $('#game-name').text(data.name);
+                 $('#game-img').attr("src", data.background_image);
+                 $('#description').html(data.description);
+                    $('one-game').show();
+                     let scrollDiv = document.getElementById("one-game").offsetTop;
+                     window.scrollTo({ top: scrollDiv, behavior: 'smooth'});
+                 
+             });
+         })
+         .catch(function (error) {
+             alert("Unable to connect to openweathermap.org");
+         });
+    
+ }
 
-
-    apiURLlDetailed=`https://api.rawg.io/api/games/${gameSlug}?&key=${rawgKey}`;
-    fetch(apiURLlDetailed)
-        .then(function (response) {
+function GBsearch(){
+    GBUrl=`https://cors-anywhere.herokuapp.com/https://www.giantbomb.com/api/search/?api_key=${gbKey}&format=json&query="${game}"&resources=game&limit=1`;
+    fetch(GBUrl)
+    .then(function (response) {
+        // request was successful
+        if (response.ok) {
             response.json().then(function (data) {
+             
+                // search details for this game
                 console.log(data);
+                if (data.results.length>0){
+                fetch(`https://cors-anywhere.herokuapp.com/https://www.giantbomb.com/api/game/${data.results[0].guid}/?api_key=${gbKey}&format=json`)
+                .then(function(response){
+                    return response.json();
+                })
+                .then(function(data){
+                    console.log(data);
+                    if (data.results.characters){
+                        for (i=0;i<data.results.characters.length;i++){
+                           let characterId=data.results.characters[i].id;
+                           console.log("id "+characterId);
+                            //get guide
+                           // console.log(`https://cors-anywhere.herokuapp.com/https://www.giantbomb.com/api/characters/?filter=id:${characterId}&api_key=${gbKey}}&format=json`)
+                            fetch(`https://cors-anywhere.herokuapp.com/https://www.giantbomb.com/api/characters/?filter=id:${characterId}&api_key=${gbKey}&format=json`)
+                            .then(function(response){
+                                return response.json();
+                        })
+                        .then(function(data){
+                            console.log(data);
+                            console.log("---");
+                        //     <div class="p-6  border-b  bg-gray-800 border-gray-700 sm:px-20">
+                        //     <div id="game-name" class="mt-8 text-2xl text-gray-200"> </div>
+                        //     <div class="flex flex-col lg:flex-row  space-x-4  mx-auto  flex-auto">
+                        //         <img id="game-img" alt="after!!" class="object-contain w-1/2 align-top ">
+                        //         <div id="description" class="text-gray-200"></div>
+                        //     </div>
+                        // </div>
+                        //<div id="game-name" class="mt-8 text-2xl text-gray-200"> </div>
+                        //p-6  border-b  bg-gray-800 border-gray-700 sm:px-20
+                        let divName=$('<div>'), divInfo=$('<div>'), chDiv=$('<div>'), divCont=$('<div>');
+                        chDiv.addClass('p-6  border-b  bg-gray-800 border-gray-700 sm:px-20');
+                        
+                        divName.text(data.results[0].name);
+                        divName.addClass("mt-8 text-2xl text-gray-200");
+                        chDiv.append(divName);
+                        divInfo.addClass("flex flex-col lg:flex-row  space-x-4  mx-auto  flex-auto text-gray-200");
+                        let icon = $('<img>');
+                     icon.addClass("object-contain h-24");
+                     icon.attr("src", data.results[0].image.icon_url);
+                     divInfo.append(icon);
+                     let p1 = $('<p>'),p2 = $('<p>');
+                     p1.html(data.results[0].deck);
+                     p1.addClass("mt-2 text-sm text-gray-400");
+                     divCont.append(p1);
+                     p2.html(data.results[0].description);
+                     p2.addClass("mt-2 text-sm text-gray-400");
+                     divCont.append(p2);
+                    divInfo.append(divCont);
+                    chDiv.append(divInfo);
+                    $('#game-content').append(chDiv);
 
-              
-                // let mainDiv = $('<div>'), descDiv = $('<div>');
-                // mainDiv.addClass("text-white");
-                // let h2 = $('<h2>');
-                $('#game-name').text(data.name);
-                $('#game-img').attr("src", data.background_image);
-                $('#description').html(data.description);
-                
 
-                // mainDiv.append(h2);
-                // let jumboDiv=$('<div>');
-                //     jumboDiv.addClass("flex flex-col lg:flex-row  space-x-4  mx-auto  flex-auto");
-                    
-                //     let icon = $('<img>');
-                //     icon.addClass("object-contain w-3/5");
-                //     icon.attr("src", data.background_image);
-                    
-                //     jumboDiv.append(icon);
 
-                   
-                   
-                //     let div=$('<div>');
-                //     div.html(data.description);
-                //     descDiv.append(div);
-                //     jumboDiv.append(descDiv);
-                //     mainDiv.append(jumboDiv);
-                //     $('#one-game').append(mainDiv);
-                   // Socialgrep(data.name); 
-                   $('one-game').show();
-                    let scrollDiv = document.getElementById("one-game").offsetTop;
-                    window.scrollTo({ top: scrollDiv, behavior: 'smooth'});
-                
-            });
-        })
-        .catch(function (error) {
-            alert("Unable to connect to openweathermap.org");
+                        });
+                    }}
+                    else {NothingFound();}
+                }
+
+                );
+                }else{NothingFound(); }
         });
-   
+            
+           
+        } else {NothingFound();}
+    })
+    .catch(function (error) {
+
+        alert("Unable to connect to openweathermap.org");
+    });
+}
+let NothingFound=function(){
+    let msg = $('<p>');
+            msg.text("nothing found");
+            msg.addClass("m-8 text-center text-l text-gray-200");
+            $('#game-content').append(msg);
 }
 
-let Socialgrep=function(game){
-    SGUrl=`https://socialgrep.p.rapidapi.com/search/posts?query=${game}`;
-    fetch(SGUrl, {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "cfad5dea77mshfa65b5044f9903dp1a13d5jsn51d29068967e",
-		"x-rapidapi-host": "socialgrep.p.rapidapi.com"
-	}
-})
-.then(function (response) {
-    response.json().then(function (data) {
-        console.log(data);
-        let h4 = $('<h5>');
-         h4.text("Data from Socialgrep:");
-        $('#socialgrep').append(h4);
-        for (i=0; i<data.data.length;i++){
-            let p=$('<p>');p2=$('<p>');p3=$('<p>');
-            if (data.data[i].selftext){
-                p.text(data.data[i].domain+ " post");
-                p2.text(data.data[i].title);
-                p3.text(data.data[i].selftext);
-                $('#socialgrep').append(p);
-                $('#socialgrep').append(p2);
-                $('#socialgrep').append(p3);
-                $('#socialgrep').append($('</br>'));
-            }
-        }
-    });
-})
-.catch(function (error) {
-    alert("Unable to connect to openweathermap.org");
-});
-}
+$('#characters').click(GBsearch);
 $('#search').click(GetWeather);
